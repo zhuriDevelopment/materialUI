019188d62eb0c81f0b78b2d48fcd5399593a0fc5
Titanxu <titanxuhaha@gmail.com>
2019-03-05 12:10:54 +0800

    修改了文件缩进为tab=2，并统一一些字体大小为18px，容器最小长度为90px，overflow:hidden

--- src/components/MaterialInfoListComponents/MaterialInfoSearchNaviCard.vue
+++ src/components/MaterialInfoListComponents/MaterialInfoSearchNaviCard.vue
@@ -1,190 +1,190 @@
 <template>
-    <!-- 搜索导航栏卡片 -->
-    <el-card class="box-card">
-        <!-- 一行的布局 -->
-        <el-row :gutter="20" type="flex">
-            <!-- 物料分类输入框 -->
-            <el-col :span="6">
-                <div class="bar">
-                    <div class="title">
-                        物料分类:
-                    </div>
-                    <el-select class="barSelector" v-model="naviMtrCtgValue" placeholder="请选择">
-                        <el-option
-                        v-for="item in materialCategInfo"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value">
-                        </el-option>
-                    </el-select>
-                </div>
-            </el-col>
+  <!-- 搜索导航栏卡片 -->
+  <el-card class="box-card">
+    <!-- 一行的布局 -->
+    <el-row :gutter="20" type="flex">
+      <!-- 物料分类输入框 -->
+      <el-col :span="6">
+        <div class="bar">
+          <div class="title">
+            物料分类:
+          </div>
+          <el-select class="barSelector" v-model="naviMtrCtgValue" placeholder="请选择">
+            <el-option
+            v-for="item in materialCategInfo"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
 
-            <!-- 物料名称输入框 -->
-            <el-col :span="6">
-                <div class="bar">
-                    <div class="title">
-                    物料名称:
-                    </div>
-                    <el-input v-model="mtrNameInput" placeholder="请输入内容"></el-input>
-                </div>
-            </el-col>
+      <!-- 物料名称输入框 -->
+      <el-col :span="6">
+        <div class="bar">
+          <div class="title">
+          物料名称:
+          </div>
+          <el-input v-model="mtrNameInput" placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
 
-            <!-- SKU编码输入框 -->
-            <el-col :span="6">
-                <div class="bar">
-                    <div class="title">
-                        SKU编码:
-                    </div>
-                    <el-input v-model="mtrSKUCodeInput" placeholder="请输入内容"></el-input>
-                </div>
-            </el-col>
+      <!-- SKU编码输入框 -->
+      <el-col :span="6">
+        <div class="bar">
+          <div class="title">
+            SKU编码:
+          </div>
+          <el-input v-model="mtrSKUCodeInput" placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
 
-           
-            <el-col :span="6" class="buttonCol">
-                <!-- 高级搜索按钮 -->
-                <el-button  plain @click="advancedSearchButtonClick">
-                    高级搜索
-                </el-button>
-                
-                <!-- 搜索按钮 -->
-                <el-button type="primary"  icon="el-icon-search" @click="searchClick()">
-                    搜索
-                </el-button>
-
-                <!-- 新建物料按钮 -->
-                <el-button type="primary" icon="el-icon-plus">
-                    新建物料
-                </el-button>
-            </el-col>
-        </el-row>
+       
+      <el-col :span="6" class="buttonCol">
+        <!-- 高级搜索按钮 -->
+        <el-button  plain @click="advancedSearchButtonClick">
+          高级搜索
+        </el-button>
         
-        <!-- 高级搜索信息项 -->
-        <el-row :gutter="20" type="flex" v-show="advSrchItmShw">
-            <!-- 时间输入框 -->
-            <el-col :span="6">
-                <div class="bar" v-show="advSrchItmShw">
-                    <div class="title">
-                        时间:
-                    </div>
-                    <el-select class="barSelector" v-model="naviMtrCtgValue" placeholder="请选择">
-                        <el-option
-                        v-for="item in materialCategInfo"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value">
-                        </el-option>
-                    </el-select>
-                </div>
-            </el-col>
+        <!-- 搜索按钮 -->
+        <el-button type="primary"  icon="el-icon-search" @click="searchClick()">
+          搜索
+        </el-button>
 
-            <!-- SPU编码输入框 -->
-            <el-col :span="6">
-                <div class="bar">
-                    <div class="title">
-                    SPU编码:
-                    </div>
-                    <el-input v-model="mtrCodeInput" placeholder="请输入内容"></el-input>
-                </div>
-            </el-col>
+        <!-- 新建物料按钮 -->
+        <el-button type="primary" icon="el-icon-plus">
+          新建物料
+        </el-button>
+      </el-col>
+    </el-row>
+    
+    <!-- 高级搜索信息项 -->
+    <el-row :gutter="20" type="flex" v-show="advSrchItmShw">
+      <!-- 时间输入框 -->
+      <el-col :span="6">
+        <div class="bar" v-show="advSrchItmShw">
+          <div class="title">
+            时间:
+          </div>
+          <el-select class="barSelector" v-model="naviMtrCtgValue" placeholder="请选择">
+            <el-option
+            v-for="item in materialCategInfo"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
 
-            <!-- 设计图号输入框 -->
-            <el-col :span="6">
-                <div class="bar">
-                    <div class="title">
-                        设计图号:
-                    </div>
-                    <el-input v-model="mtrHelperCodeInput" placeholder="请输入内容"></el-input>
-                </div>
-            </el-col>
-        </el-row>
+      <!-- SPU编码输入框 -->
+      <el-col :span="6">
+        <div class="bar">
+          <div class="title">
+          SPU编码:
+          </div>
+          <el-input v-model="mtrCodeInput" placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
 
-        <el-row :gutter="20" type="flex" v-show="advSrchItmShw">
-            <!-- 设计版本输入框 -->
-            <el-col :span="6">
-                <div class="bar">
-                    <div class="title">
-                        设计版本:
-                    </div>
-                    <el-select class="barSelector" v-model="naviMtrCtgValue" placeholder="请选择">
-                        <el-option
-                        v-for="item in materialCategInfo"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value">
-                        </el-option>
-                    </el-select>
-                </div>
-            </el-col>
+      <!-- 设计图号输入框 -->
+      <el-col :span="6">
+        <div class="bar">
+          <div class="title">
+            设计图号:
+          </div>
+          <el-input v-model="mtrHelperCodeInput" placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
+    </el-row>
 
-            <!-- 来源输入框 -->
-            <el-col :span="6">
-                <div class="bar">
-                    <div class="title">
-                        来源:
-                    </div>
-                    <el-input v-model="mtrCodeInput" placeholder="请输入内容"></el-input>
-                </div>
-            </el-col>
-        </el-row> 
-    </el-card>
+    <el-row :gutter="20" type="flex" v-show="advSrchItmShw">
+      <!-- 设计版本输入框 -->
+      <el-col :span="6">
+        <div class="bar">
+          <div class="title">
+            设计版本:
+          </div>
+          <el-select class="barSelector" v-model="naviMtrCtgValue" placeholder="请选择">
+            <el-option
+            v-for="item in materialCategInfo"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
+
+      <!-- 来源输入框 -->
+      <el-col :span="6">
+        <div class="bar">
+          <div class="title">
+            来源:
+          </div>
+          <el-input v-model="mtrCodeInput" placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
+    </el-row> 
+  </el-card>
 </template>
 
 <style lang="less" scoped>
-    .box-card {
-        width: 100%;
-        min-width: 900px;
-        margin: 1% 1%;
-        
-        .el-row{
-            margin: 10px ;
-            .buttonCol{
-                display: flex;
-            }
-        }
+  .box-card {
+    width: 100%;
+    min-width: 900px;
+    margin: 1% 1%;
+    
+    .el-row{
+      margin: 10px ;
+      .buttonCol{
+        display: flex;
+      }
+    }
 
-        .bar{
-            display: flex;
-            flex-direction: row;
-            flex-wrap: nowrap;
-            align-items: center;
-            min-width: 50%;
+    .bar{
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      align-items: center;
+      min-width: 50%;
 
-            .title {
-                font-size: 18px;
-                min-width: 90px;
-                text-align: center;
-                margin: 5px;
-            }
+      .title {
+        font-size: 18px;
+        min-width: 90px;
+        text-align: center;
+        margin: 5px;
+      }
 
-            .barSelector{
-                min-width: 60%;
-            }
-        }
-    } 
+      .barSelector{
+        min-width: 60%;
+      }
+    }
+  } 
 </style>
 
 <script>
-    import MaterialInfoSearchNaviCardAPIs from '@/api/MaterialInfoListAPIs/MaterialInfoSearchNaviCardAPIs';
+  import MaterialInfoSearchNaviCardAPIs from '@/api/MaterialInfoListAPIs/MaterialInfoSearchNaviCardAPIs';
 
-    export default {
-        name:"MaterialInfoSearchNaviCard",
+  export default {
+    name:"MaterialInfoSearchNaviCard",
 
-        data() {
-            return {
-                materialCategInfo : MaterialInfoSearchNaviCardAPIs.getMaterialCategInfo(),
-                naviMtrCtgValue:'',
-                mtrNameInput:'',
-                mtrSKUCodeInput:'',
-                advSrchItmShw: false
-            }
-        },
+    data() {
+      return {
+        materialCategInfo : MaterialInfoSearchNaviCardAPIs.getMaterialCategInfo(),
+        naviMtrCtgValue:'',
+        mtrNameInput:'',
+        mtrSKUCodeInput:'',
+        advSrchItmShw: false
+      }
+    },
 
-        methods:{
-            advancedSearchButtonClick() {
-                this.advSrchItmShw = !this.advSrchItmShw;
-                console.log("activeNames");
-            }
-        }
+    methods:{
+      advancedSearchButtonClick() {
+        this.advSrchItmShw = !this.advSrchItmShw;
+        console.log("activeNames");
+      }
     }
+  }
 </script>
--- src/components/MaterialInfoModify/MaterialDefinationCard.vue
+++ src/components/MaterialInfoModify/MaterialDefinationCard.vue
@@ -1,150 +1,150 @@
 <template>
 <!-- 物料定义卡 -->
-    <!--物料定义的展示编辑表格-->
-    <el-card class="box-card" >
-        <el-table
-            border="true"
-            ref="singleTable"
-            :data="tableData"
-            highlight-current-row
-            @current-change="handleCurrentChange"
-            style="width: 100%">
-                <!--索引列-->
-                <el-table-column
-                label="序号"
-                type="index"
-                width="50">
-                </el-table-column>
+  <!--物料定义的展示编辑表格-->
+  <el-card class="box-card" >
+    <el-table
+    border="true"
+    ref="singleTable"
+    :data="tableData"
+    highlight-current-row
+    @current-change="handleCurrentChange"
+    style="width: 100%">
+      <!--索引列-->
+      <el-table-column
+      label="序号"
+      type="index"
+      width="50">
+      </el-table-column>
 
-                <!--第一列-->
-                <el-table-column
-                prop="date"
-                label="物料编码"
-                width="120">
-                    <template slot-scope="scope">
-                            <el-input
-                            placeholder="请输入内容"
-                            v-model="input1"
-                            :disabled="false">
-                            </el-input>
-                    </template>
-                </el-table-column>
+      <!--第一列-->
+      <el-table-column
+      prop="date"
+      label="物料编码"
+      width="120">
+        <template slot-scope="scope">
+          <el-input
+          placeholder="请输入内容"
+          v-model="input1"
+          :disabled="false">
+          </el-input>
+        </template>
+      </el-table-column>
 
-                <!--第二列-->
-                <el-table-column
-                prop="name"
-                label="物料名称"
-                width="120">
-                    <template slot-scope="scope">
-                            <el-input
-                            placeholder="请输入内容"
-                            v-model="input1"
-                            :disabled="false">
-                            </el-input>
-                    </template>
-                </el-table-column>
+      <!--第二列-->
+      <el-table-column
+      prop="name"
+      label="物料名称"
+      width="120">
+        <template slot-scope="scope">
+          <el-input
+          placeholder="请输入内容"
+          v-model="input1"
+          :disabled="false">
+          </el-input>
+        </template>
+      </el-table-column>
 
-                <!--第三列-->
-                <el-table-column
-                prop="name"
-                label="旧物料名称">
-                    <template slot-scope="scope">
-                            <el-input
-                            placeholder="请输入内容"
-                            v-model="input1"
-                            :disabled="false">
-                            </el-input>
-                    </template>
-                </el-table-column>
+      <!--第三列-->
+      <el-table-column
+      prop="name"
+      label="旧物料名称">
+        <template slot-scope="scope">
+          <el-input
+          placeholder="请输入内容"
+          v-model="input1"
+          :disabled="false">
+          </el-input>
+        </template>
+      </el-table-column>
 
-                <!--第四列-->
-                <el-table-column
-                prop="name"
-                label="条形码">
-                    <template slot-scope="scope">
-                            <el-input
-                            placeholder="请输入内容"
-                            v-model="input1"
-                            :disabled="false">
-                            </el-input>
-                    </template>
-                </el-table-column>
+      <!--第四列-->
+      <el-table-column
+      prop="name"
+      label="条形码">
+        <template slot-scope="scope">
+          <el-input
+          placeholder="请输入内容"
+          v-model="input1"
+          :disabled="false">
+          </el-input>
+        </template>
+      </el-table-column>
 
-                <!--第五列-->
-                <el-table-column
-                prop="name"
-                label="规格1">
-                    <template slot-scope="scope">
-                            <el-input
-                            placeholder="请输入内容"
-                            v-model="input1"
-                            :disabled="false">
-                            </el-input>
-                    </template>
-                </el-table-column>
+      <!--第五列-->
+      <el-table-column
+      prop="name"
+      label="规格1">
+        <template slot-scope="scope">
+          <el-input
+          placeholder="请输入内容"
+          v-model="input1"
+          :disabled="false">
+          </el-input>
+        </template>
+      </el-table-column>
 
-                <!--第六列-->
-                <el-table-column
-                prop="name"
-                label="规格2">
-                    <template slot-scope="scope">
-                            <el-input
-                            placeholder="请输入内容"
-                            v-model="input1"
-                            :disabled="false">
-                            </el-input>
-                    </template>
-                </el-table-column>
+      <!--第六列-->
+      <el-table-column
+      prop="name"
+      label="规格2">
+        <template slot-scope="scope">
+          <el-input
+          placeholder="请输入内容"
+          v-model="input1"
+          :disabled="false">
+          </el-input>
+        </template>
+      </el-table-column>
 
-                <!--第七列-->
-                <el-table-column
-                prop="name"
-                label="规格N">
-                    <template slot-scope="scope">
-                            <el-input
-                            placeholder="请输入内容"
-                            v-model="input1"
-                            :disabled="false">
-                            </el-input>
-                    </template><a href=""></a>
-                </el-table-column>
+      <!--第七列-->
+      <el-table-column
+      prop="name"
+      label="规格N">
+        <template slot-scope="scope">
+          <el-input
+          placeholder="请输入内容"
+          v-model="input1"
+          :disabled="false">
+          </el-input>
+        </template><a href=""></a>
+      </el-table-column>
 
-                <!--操作栏列-->
-                <el-table-column
-                fixed="right"
-                label="操作"
-                width="100">
-                    <template slot-scope="scope">
-                        <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
-                        <el-button type="text" size="small">删除</el-button>
-                    </template>
-                </el-table-column> 
-        </el-table>
-    </el-card>
+      <!--操作栏列-->
+      <el-table-column
+      fixed="right"
+      label="操作"
+      width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column> 
+    </el-table>
+  </el-card>
 </template>
 
 <style <style lang="less" scoped>
-    .box-card{
-        min-width: 900px;
-    }
+  .box-card{
+    min-width: 900px;
+  }
 </style>
 
 <script>
-export default {
+  export default {
     name:"MaterialDefinationCard",
     data() {
       return {
-            tableData: [{
-                date: '2016-05-03',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-                }],
-            input:'',
-            currentRow: null
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+          }],
+        input:'',
+        currentRow: null
       }
     },
-}
+  }
 </script>
